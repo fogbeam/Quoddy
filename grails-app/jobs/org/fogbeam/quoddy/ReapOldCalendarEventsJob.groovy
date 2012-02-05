c9d50644672ee13d6c5d48f9779781f2036b897a
Phillip Rhodes <motley.crue.fan@gmail.com>
2012-02-05 01:18:49 -0500

    starts adding scheduled jobs for managing calendar events

---
+++ grails-app/jobs/org/fogbeam/quoddy/ReapOldCalendarEventsJob.groovy
@@ -0,0 +1,25 @@
+package org.fogbeam.quoddy
+
+
+import java.text.SimpleDateFormat 
+
+
+class ReapOldCalendarEventsJob 
+{
+	
+	def group = "MyGroup";
+	def volatility = false;
+	
+	static triggers = {
+	}
+	
+    def execute() 
+	{
+     	
+		Date now = new Date();
+		SimpleDateFormat sdf = SimpleDateFormat.getDateTimeInstance();
+		
+		println( "TRIGGER: Reaping old CalendarEvent instances: ${sdf.format( now )}" );
+
+	}
+}
---
+++ grails-app/jobs/org/fogbeam/quoddy/UpdateCalendarFeedsJob.groovy
@@ -0,0 +1,28 @@
+package org.fogbeam.quoddy
+
+
+import java.text.SimpleDateFormat 
+
+
+class UpdateCalendarFeedsJob 
+{
+	
+	def group = "MyGroup";
+	def volatility = false;
+	
+	static triggers = {
+	}
+	
+    def execute() 
+	{
+     	// get a list of all the active CalendarFeed objects
+		
+		// for each feed:
+		
+			// download the contents of the feed, and parse out all of the VEVENTs
+		
+			// for each VEVENT create a CalendarEvent instance with the CalendarFeed owner
+			// as the ownere of the VEVENT
+		
+	}
+}
