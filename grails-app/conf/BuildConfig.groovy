2703da200abbaf45cb93f81e187b510db8f0a095
Phillip Rhodes (mindcrime) <motley.crue.fan@gmail.com>
2013-02-10 00:00:07 -0500

    update excludes in build-config for slf4j stuff we don't want

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,7 +6,7 @@ grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits( "global" ) {
         // uncomment to disable ehcache
-        // excludes 'ehcache'
+		excludes 'slf4j-api', 'slf4j-log4j12', 'jcl-over-slf4j', 'jul-to-slf4j'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {        
