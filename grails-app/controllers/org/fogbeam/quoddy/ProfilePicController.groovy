981c23d52a530844375602c1e7dd78cf020bf405
Phillip Rhodes <motley.crue.fan@gmail.com>
2015-08-27 22:17:27 -0400

    replace println with use of log object

--- grails-app/controllers/org/fogbeam/quoddy/ProfilePicController.groovy
+++ grails-app/controllers/org/fogbeam/quoddy/ProfilePicController.groovy
@@ -11,22 +11,22 @@ class ProfilePicController
 		}
 		else 
 		{
-			println( "params.userId found: ${params.id}");
+			log.debug( "params.userId found: ${params.id}");
 			String quoddyHome = System.getProperty( "quoddy.home" );
 			String filePath = 
 				"${quoddyHome}/profilepics/${params.id}/${params.id}_profile_thumbnail48x48.jpg";
-			println( "filepath is ${filePath}");
+			log.debug( "filepath is ${filePath}");
 			File thumbnailFile = new File(filePath);
 			byte[] image = thumbnailFile.getBytes();
-			println( "image size: ${image.length}");
+			log.debug( "image size: ${image.length}");
 			response.setHeader("Content-Type", "image/jpeg");
 			response.setHeader("Content-Disposition", "attachment;filename=mypic.jpeg");
 			response.setHeader("Content-Length", "${image.length}");
 			// println image;
-			println "class: " + response.outputStream.getClass().getName();
+			log.debug( "class: " + response.outputStream.getClass().getName());
 			response.outputStream.write( image );
 			response.outputStream.flush();
-			println "done";
+			log.debug( "done");
 			return;
 		}
 	}
