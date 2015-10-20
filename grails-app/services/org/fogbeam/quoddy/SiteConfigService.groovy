64a8d03fd85e9fbfa87f7c7244487b646564e8a8
Phillip Rhodes <motley.crue.fan@gmail.com>
2015-10-20 19:08:32 -0400

    replace use of println with use of log object

--- grails-app/services/org/fogbeam/quoddy/SiteConfigService.groovy
+++ grails-app/services/org/fogbeam/quoddy/SiteConfigService.groovy
@@ -7,17 +7,17 @@ class SiteConfigService {
 
 	public String getSiteConfigEntry( final String name )
 	{
-		println "siteConfigEntry request for ${name}";
+		log.debug( "siteConfigEntry request for ${name}");
 		
 		SiteConfigEntry entry = SiteConfigEntry.findByName( name );
 		
 		if( entry != null )
 		{
-			println "found a value for ${name}";	
+			log.debug( "found a value for ${name}");	
 		}
 		else
 		{
-			println "found no value for ${name}";
+			log.debug( "found no value for ${name}" );
 		}
 		
 		return entry?.value;
