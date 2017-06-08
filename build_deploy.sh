12e05a99bedc06c6e840c7bde69cf4d7c615a394
root <root@ip-10-0-0-20.ec2.internal>
2017-06-08 00:01:50 +0000

    flesh out build_deploy.sh

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+
+service tomcat stop
+sleep 5s
+service tomcat stop
+sleep 5s
+
+rm -rf /usr/share/tomcat/webapps/quoddy/
+rm -f /usr/share/tomcat/webapps/quoddy.war
+rm -f /usr/share/tomcat/logs/*
+rm -f /opt/fogcutter/quoddy/quoddy.log
+
+grails clean
+
+grails war
+
+cp target/quoddy-0.0.1.war /usr/share/tomcat/webapps/quoddy.war
+
+service tomcat start
