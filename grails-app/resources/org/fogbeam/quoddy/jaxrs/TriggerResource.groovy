75341348b3bf93d602bc7209a91cfc6089c0b86a
Phillip Rhodes <motley.crue.fan@gmail.com>
2017-06-10 00:21:59 -0400

    add Resource for managing ScheduledJob Triggers

---
+++ grails-app/resources/org/fogbeam/quoddy/jaxrs/TriggerResource.groovy
@@ -0,0 +1,66 @@
+package org.fogbeam.quoddy.jaxrs
+
+import static org.grails.jaxrs.response.Responses.*
+import groovy.json.JsonSlurper
+
+import javax.ws.rs.Consumes
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response.Status
+
+import org.codehaus.groovy.grails.commons.GrailsClass
+import org.quartz.Trigger
+import org.quartz.impl.triggers.SimpleTriggerImpl
+
+@Path( "api/scheduledjob/trigger" )
+class TriggerResource 
+{
+	@POST
+	@Consumes( "application/json")
+	@Produces( "text/plain")
+	public Response addTrigger( String inputData ) 
+	{
+		
+		
+		println "inputData: \n ${inputData}";
+		log.info( "inputData:\n ${inputData}");
+
+		JsonSlurper jsonSlurper = new JsonSlurper();
+		def jsonObject = jsonSlurper.parseText(inputData);
+
+		
+		String jobGroup = jsonObject.jobGroup;
+		String jobName = jsonObject.jobName;
+		
+		String triggerName = jsonObject.triggerName;
+		String triggerGroup = jsonObject.triggerGroup; 
+		int repeatCount = Integer.parseInt(jsonObject.repeatCount);
+		long recurrenceInterval = Long.parseLong(jsonObject.recurrenceInterval);
+		
+		
+		log.info( "adding Trigger for jobName: ${jobName}" );
+
+		
+		GrailsClass jobClass = grailsApplication.getArtefact( "Job", jobName );
+		
+		if( jobClass == null )
+		{
+			log.error( "Could not load GrailsClass for ${jobName}" );
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+		else
+		{
+			// SimpleTrigger(String name, String group, int repeatCount, long repeatInterval)
+			
+			Trigger trigger = new SimpleTriggerImpl( triggerName, triggerGroup, repeatCount, recurrenceInterval);
+			jobClass.newInstance().schedule( trigger );
+		}
+				
+			
+		
+		ok( "OK" );
+	}
+	
+}
