f56112a40d98547af6bdf3b79383eae97f266e87
Phillip Rhodes <motley.crue.fan@gmail.com>
2017-06-18 04:55:09 -0400

    remove the quoddy.log when running this script, and (optionally) clean up the RSS feed items

--- runquoddy.sh
+++ runquoddy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+rm /opt/fogcutter/quoddy/quoddy.log
+
+# psql -U postgres -d quoddy -f sql/clean_rss.sql
+
 grails clean; grails -Dserver.port=8180 -Dquoddy.home=/opt/fogcutter/quoddy run-app
 
