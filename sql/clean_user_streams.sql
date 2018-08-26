4d83821925f7e59321468ee1586b403aa9229256
Phillip Rhodes <motley.crue.fan@gmail.com>
2018-08-26 06:28:56 -0400

    cleanup before merging the new grails3 branch

--- sql/clean_user_streams.sql
+++
@@ -1,6 +0,0 @@
-﻿-- delete from user_stream_definition_event_type tbl where tbl.user_stream_definition_event_types_included_id in ( select id from user_stream_definition where name <> 'Default')
--- delete from user_stream_definition_subscription_uuids_included tbl where tbl.user_stream_definition_id in ( select id from user_stream_definition where name <> 'Default')
---delete from user_stream_definition_user_list_uuids_included tbl where tbl.user_stream_definition_id in ( select id from user_stream_definition where name <> 'Default')
--- delete from user_stream_definition_user_uuids_included tbl where tbl.user_stream_definition_id in ( select id from user_stream_definition where name <> 'Default')
--- delete from user_stream_definition_user_group_uuids_included tbl where tbl.user_stream_definition_id in ( select id from user_stream_definition where name <> 'Default')
--- delete from user_stream_definition where name <> 'Default';
