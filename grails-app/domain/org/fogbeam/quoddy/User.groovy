e751a944e20df49941f490fdb314e3f3766a116c
Phillip Rhodes <motley.crue.fan@gmail.com>
2018-10-05 23:15:46 -0400

    take out the old 'permissions' stuff from the User object, this is managed differently now

--- grails-app/domain/org/fogbeam/quoddy/User.groovy
+++ grails-app/domain/org/fogbeam/quoddy/User.groovy
@@ -88,7 +88,7 @@ class User implements Serializable, UserDetails
 	
 	static fetchMode = [roles: 'eager', permissions:'eager'];
 	
-    static hasMany = [oldStatusUpdates:StatusUpdate, roles: AccountRole, permissions: String, streams:UserStreamDefinition];
+    static hasMany = [oldStatusUpdates:StatusUpdate, roles: AccountRole, streams:UserStreamDefinition];
 	static mappedBy = [oldStatusUpdates:'creator']
 
     
