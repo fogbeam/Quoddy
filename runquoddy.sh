1230606abe93324700560b8a8a4ffa4c4c70b49e
Phillip Rhodes <motley.crue.fan@gmail.com>
2023-03-26 18:39:33 -0400

    Changes to support smoke testing on the Jenkins CI server

--- runquoddy.sh
+++ runquoddy.sh
@@ -6,5 +6,13 @@ rm /opt/fogcutter/quoddy/quoddy.log
 
 ./grailsw clean
 
-sh -c "echo $$ > pidfile; exec ./grailsw -Dserver.port=8383 -Dquoddy.home=/opt/fogcutter/quoddy -Dspring.config.location=/opt/fogcutter/quoddy/ -Drebuild.indexes=false -Dfogbeam.devmode=true -Dspring.profiles.active=test -Dgrails.env=test run-app $@"
+./grailsw -Dserver.port=8383 -Dquoddy.home=/opt/fogcutter/quoddy -Dspring.config.location=/opt/fogcutter/quoddy/ -Drebuild.indexes=false -Dfogbeam.devmode=true -Dspring.profiles.active=test -Dgrails.env=test run-app $@ &
+
+while ! nc -z localhost 8383; do
+  sleep 0.1 # wait for 1/10 of the second before check again                                                                 
+done
+
+echo "Quoddy started, writing pidfile"
+
+lsof -i6TCP:8383 -sTCP:LISTEN -t > pidfile
 
